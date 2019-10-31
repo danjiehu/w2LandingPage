040a8d8a7749056586b0cedad49a72a7dbc15f67
luangxiao <luangxiao914@gmail.com>
2019-10-31 14:08:30 +0800

    Update listener.js

--- 02-javascript/exercises/dom-events/02-My-First-Event-Listener/lib/listener.js
+++ 02-javascript/exercises/dom-events/02-My-First-Event-Listener/lib/listener.js
@@ -1,1 +1,9 @@
 // TODO: React to a click on the button!
+let button = document.querySelector("#clickme"); 
+button.addEventListener("click", (event) => {
+  // Do something（回调 callback）
+  event.currentTarget.classList.add("disabled");
+  event.currentTarget.innerHTML = "Bingo!";
+  let audio= new Audio("../02-My-First-Event-Listener/sound.mp3");//这里的路径写上mp3文件在项目中的绝对路径
+  audio.play();
+});
