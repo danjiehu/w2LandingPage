c03e2727d332eb9f7d8660fa1c23513b2bfaff95
Dounan Hu <dounan1@gmail.com>
2019-11-29 04:23:18 +0800

    added solution for payment

---
+++ 05-advanced_baas/solutions/payment/pages/orders/orders.js
@@ -0,0 +1,115 @@
+//orders.js
+
+Page({
+  data: {
+
+  },
+  // binded to clicking on the order 'ready' button
+  readyOrder(event) {
+    const page = this;
+    const data = event.currentTarget.dataset;
+    const id = data.id;
+
+    console.log(id);
+
+    let tableName = 'order'
+    let Order = new wx.BaaS.TableObject(tableName)
+    let order = Order.getWithoutData(id)
+
+    order.set("state", "ready").update().then(res => {
+      wx.reLaunch({
+        url: '/pages/orders/orders',
+      })
+    })
+  },
+  // binded to clicking on the order 'deliver' button
+  deliverOrder(event) {
+    const page = this;
+    const data = event.currentTarget.dataset;
+    const id = data.id;
+    const currentUser = page.data.currentUser;
+
+    console.log(id);
+
+    let tableName = 'order';
+
+    let Order = new wx.BaaS.TableObject(tableName);
+    let order = Order.getWithoutData(id);
+
+    order.set("state", "delivering");
+    order.set("deliverer", currentUser.id.toString());
+    order.update().then(res => {
+      wx.reLaunch({
+        url: '/pages/orders/orders',
+      })
+    })
+  },
+  deliveredOrder(event) {
+    const page = this;
+    const data = event.currentTarget.dataset;
+    const id = data.id;
+
+    console.log(id);
+
+    let tableName = 'order'
+    let Order = new wx.BaaS.TableObject(tableName)
+    let order = Order.getWithoutData(id)
+
+    order.set("state", "delivered").update().then(res => {
+      wx.reLaunch({
+        url: '/pages/orders/orders',
+      })
+    })
+  },
+  onLoad: function (options) {
+    const page = this;
+    console.log(options)
+
+    wx.BaaS.auth.getCurrentUser().then(user => {
+      // user 为 currentUser 对象
+
+      console.log(user)  
+      page.setData({
+        currentUser: user
+      })
+
+      page.loadOrder();
+      
+
+    }, error => {
+      console.log(error)
+      page.setData({
+        currentUser: null
+      })
+      wx.switchTab({
+        url: '/pages/user/user' // logged in
+      });
+    })
+  },
+  loadOrder: function() {
+    const page = this;
+
+    let tableName = 'order'
+
+    let Order = new wx.BaaS.TableObject(tableName)
+
+    var query = new wx.BaaS.Query()
+
+    const isDeliverer = page.data.currentUser.get('role') == 'deliverer';
+
+    if (isDeliverer) {
+      query.in('state', ['ready', 'delivering']);
+      Order.setQuery(query);
+    }
+
+    Order.expand(["product", "deliverer"]).find().then(page.getRequestData)
+
+  },
+  getRequestData: function (res) {
+    const data = res.objects;
+    console.log(res);
+    this.setData({
+      orders: res.data.objects
+    });
+  },
+})
